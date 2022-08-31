da9340f50a48aa68c531585e023ca3714dbc90a7
罗鑫杰 <854484616@qq.com>
2022-08-31 09:59:06 +0800

    hot-fix commit2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,5 +9,6 @@ public class App {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
         System.out.println("hello git1");
+        System.out.println("hello hot-fix test");
     }
 }
